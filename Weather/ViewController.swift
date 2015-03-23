072794adb1cd6849c02ab02ae8a1fa94f3f06534
Joshua Raichur <hello@joshuaraichur.com>
2015-03-23 22:00:28 +0530

    working

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,10 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userCity: UITextField!
+    
+    @IBAction func findWeather(sender: AnyObject) {
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    func showError() {
+        resultLabel.text = "Unable to find weather for" + userCity.text
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/" + userCity.text.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        if url != nil {
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: {(data, response, error) -> Void in
+                
+                var urlError = false
+                var weather = ""
+                
+                if error == nil {
+                    var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding) as NSString!
+                    println(urlContent)
+                    var urlContentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+                    if urlContentArray.count > 0 {
+                        var weatherArray = urlContentArray[1].componentsSeparatedByString("</span>")
+                        weather = weatherArray[0] as String
+                        weather = weather.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        
+                    } else {
+                        urlError = true
+                    }
+                    
+                } else {
+                   urlError = true
+                }
+                
+                dispatch_async(dispatch_get_main_queue()){
+                    if urlError == true {
+                        self.showError()
+                    } else {
+                        self.resultLabel.text = weather
+                    }
+                }
+                
+            })
+            
+            task.resume()
+            
+        } else {
+            showError()
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
